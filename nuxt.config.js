83680ce641b1794295da4bb75c384d692c84f719
Paul Hendry <paul@pshendry.com>
2019-12-23 17:10:10 -0500

    Change page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   head: {
-    title: 'IL-2 Intel',
+    title: 'IL-2 Aircraft Identification',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
