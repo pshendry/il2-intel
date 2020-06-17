a99d4656d6316b02cdc9c76555e735a726dcba44
Paul Hendry <paul@pshendry.com>
2020-06-16 21:44:26 -0400

    Add message to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>TODO</div>
+  <div class="home-msg-container">
+    <div>
+      <p>
+        Tools for flying in
+        <a
+          href="https://il2sturmovik.com/about"
+          target="_blank"
+          rel="noreferrer noopener"
+        >IL-2 Sturmovik</a>!
+      </p>
+      <p>Select an option from the top menu to begin</p>
+    </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+.home-msg-container {
+  display: flex;
+  height: calc(100vh - 56px);
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  opacity: 50%;
+}
+</style>
